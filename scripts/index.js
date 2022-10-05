c4992efa1c3613a2d9f2bbb086994a21b284db56
Vladislav Nikitin <vlad.nikitin1997@bk.ru>
2022-10-05 03:09:39 +0300

    Ревью2

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,37 @@
 // Плучить элементы
 
-const popupOpenedButton = document.querySelector('.profile-info__open-popup');
-const popup = document.querySelector('.popup');
-const popupClouseButton = popup.querySelector('.popup__clouse');
+const popupOpenedButton = document.querySelector(".profile-info__open-popup");
+const popup = document.querySelector(".popup");
+const popupClouseButton = popup.querySelector(".popup__clouse");
 
-let profileName = document.querySelector('.profile-info__name');
-let profileActivity = document.querySelector('.profile-info__activity');
+let profileName = document.querySelector(".profile-info__name");
+let profileActivity = document.querySelector(".profile-info__activity");
 
-let form = document.querySelector('.popup__input');
+let form = document.querySelector(".form");
 
-let nameInput = document.querySelector('.popup__text_type_name');
-let activityInput = document.querySelector('.popup__text_type_activity');
-let formSaveButton = document.querySelector('.form__submit-btn_action_save');
+let nameInput = document.querySelector("#name");
+let activityInput = document.querySelector("#activity");
 
-profileName.textContent = 'Жак-Ив Кусто';
-profileActivity.textContent = 'Исследователь океана';
+profileName.textContent = "Жак-Ив Кусто";
+profileActivity.textContent = "Исследователь океана";
 
-function clickForm() {
-    nameInput.value = '';
-    activityInput.value = ''; 
-};
-nameInput.addEventListener("click", clickForm);
+function popupAdd() {
+  popup.classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  activityInput.value = profileActivity.textContent;
+}
 
+function popupRemove() {
+  popup.classList.remove("popup_opened");
+}
 
- function popupAdd() {
-    popup.classList.add("popup_opened");
-};
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  popupRemove();
+  profileName.textContent = nameInput.value;
+  profileActivity.textContent = activityInput.value;
+}
 
- function popupRemove() {
-    popup.classList.remove("popup_opened");
-};
-
-popupOpenedButton.addEventListener("click", popupAdd);
 popupClouseButton.addEventListener("click", popupRemove);
-
-formSaveButton.addEventListener("click", formSubmitHandler);
-formSaveButton.addEventListener("click", popupRemove);
-
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    profileName.textContent = nameInput.value;
-    profileActivity.textContent = activityInput.value; 
-};       
-
-form.addEventListener("submit", formSubmitHandler); 
+popupOpenedButton.addEventListener("click", popupAdd);
+form.addEventListener("submit", formSubmitHandler);
